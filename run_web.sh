6cf7e69657ec95794d7bbe5385c00821ff474ead
go-while <go-while@users.noreply.github.com>
2025-08-07 18:07:36 +0100

    Update run_web.sh

--- run_web.sh
+++ run_web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do
 ./copynew.sh
-./webserver -useshorthashlen 7 -import-desc preload/newsgroups.descriptions
+./webserver -useshorthashlen 7 -update-descr preload/newsgroups.descriptions
 echo "sleep 3s before restart"
 sleep 3
 done
