612cfb9422a9b251a7e0703876888380f14ca18c
go-while <go-while@users.noreply.github.com>
2025-08-10 12:42:39 +0100

    Update getUpdate.sh

--- getUpdate.sh
+++ getUpdate.sh
@@ -3,5 +3,6 @@ FILE=".update.todo"
 test ! -e "$FILE" && echo "update file not found" && exit 1
 mkdir -p tmp
 rm -f tmp/*
+test -e update.tar.gz && rm -v update.tar.gz
 wget https://pugleaf.net/cdn/go-pugleaf/update.tar.gz -O tmp/update.tar.gz && cd tmp && \
- sha256sum -c ../$FILE && tar xfvz update.tar.gz && rm update.tar.gz && mv * ../new/ && rm -v ../"$FILE"
+ sha256sum -c ../$FILE && tar xfvz update.tar.gz && mv * ../new/ && rm -v ../"$FILE"
