ce7766c44dd844684a88da8476c5c9880e6c1f68
go-while <go-while@users.noreply.github.com>
2025-08-16 14:35:22 +0100

    Update getUpdate.sh

--- getUpdate.sh
+++ getUpdate.sh
@@ -5,4 +5,6 @@ mkdir -p tmp
 rm -f tmp/*
 test -e update.tar.gz && rm -v update.tar.gz
 wget https://pugleaf.net/cdn/go-pugleaf/update.tar.gz -O tmp/update.tar.gz && cd tmp && \
- sha256sum -c ../$FILE && tar xfvz update.tar.gz && mv * ../new/ && rm -v ../"$FILE"
+ sha256sum update.tar.gz && du -b update.tar.gz && \
+ sha256sum -c ../$FILE && tar xfvz update.tar.gz && mv * ../new/
+rm -v ../"$FILE"
