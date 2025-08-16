62a8cd637f88ff82e629970d4bb709a44c50523d
go-while <go-while@users.noreply.github.com>
2025-08-16 14:35:27 +0100

    Update createUpdate.sh

--- createUpdate.sh
+++ createUpdate.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -e
+rm -f update.tar.gz
 tar -f update.tar.gz -C build -c -v -z .
 sha256sum update.tar.gz > .update
-du -hs update.tar.gz
+HASH=$(cat .update|cut -d" " -f1)
+du -b update.tar.gz
 cat .update
 exit 0
