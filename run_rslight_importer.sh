cf90dd5f7a3101738e28d3cc27d7eb81ffb569dd
go-while <go-while@users.noreply.github.com>
2025-08-16 14:35:33 +0100

    Update run_rslight_importer.sh

--- run_rslight_importer.sh
+++ run_rslight_importer.sh
@@ -1,1 +1,1 @@
-./rslight-importer -threads=4 -data data -etc /var/www/html/rocksolidbbs.com/etc/rslight -spool /var/www/html/rocksolidbbs.com/var/spool/rslight -nntphostname test.pugleaf.net
+build/rslight-importer -threads=4 -data data -etc /var/www/html/rocksolidbbs.com/etc/rslight -spool /var/www/html/rocksolidbbs.com/var/spool/rslight -nntphostname pugleaf.net
