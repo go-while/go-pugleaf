294e82ff936948817330143aec317af221d07bdc
go-while <go-while@users.noreply.github.com>
2025-08-23 23:04:36 +0100

    Update build_fetcher.sh

--- build_fetcher.sh
+++ build_fetcher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$0"
-go build -race -o build/pugleaf-fetcher -ldflags "-X main.appVersion=$(cat appVersion.txt)" ./cmd/nntp-fetcher
+GOEXPERIMENT=greenteagc go build -o build/pugleaf-fetcher -ldflags "-X main.appVersion=$(cat appVersion.txt)" ./cmd/nntp-fetcher
 exit $?
