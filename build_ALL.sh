68c4d246ab0c99288ca7d1ee4464b1ad56c73936
go-while <go-while@users.noreply.github.com>
2025-10-02 17:22:54 +0000

    Update build_ALL.sh

--- build_ALL.sh
+++ build_ALL.sh
@@ -2,19 +2,21 @@
 echo "running: $0"
 rm -v build/*
 
-./build_merge-active.sh
-./build_merge-descriptions.sh
-./build_TestMsgIdItemCache.sh
-./build_history-rebuild.sh
-./build_nntp-server.sh
-./build_fix-references.sh
-./build_fix-thread-activity.sh
+## debug tools
+#./build_merge-active.sh
+#./build_merge-descriptions.sh
+#./build_TestMsgIdItemCache.sh
+#./build_history-rebuild.sh
+#./build_fix-references.sh
+#./build_fix-thread-activity.sh
+#./build_recover-db.sh
 
+# main app
 ./build_rslight_importer.sh
 ./build_analyze.sh
 ./build_fetcher.sh
 ./build_webserver.sh
-./build_recover-db.sh
+./build_nntp-server.sh
 ./build_expire-news.sh
 ./build_nntp-transfer.sh
 ./build_post-queue.sh
